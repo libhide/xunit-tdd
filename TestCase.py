8fe6adc1caeb5d939218ff8684bf34506db5b549
Ratik Sharma <ratik96@gmail.com>
2019-03-06 00:01:43 +0530

    Add back reporting of test results (#6)
    * Add failing test for the interface we wish to build for reporting
    * Add a fake implementation to get to green
    * Convert fake implementation into a constant implementation
    * Convert the constant run_count implementation into a variable based impl
    * Add failing test to test a failed test
    * Get to green by adding a broken method that raises an Exception
    * Add missing call to run the test failure counter test

--- TestCase.py
+++ TestCase.py
@@ -1,3 +1,6 @@
+from TestResult import TestResult
+
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -6,10 +9,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.test_started()
         self.setup()
         method = getattr(self, self.name)
         method()
         self.tear_down()
+        return result
     
     def tear_down(self):
         pass
