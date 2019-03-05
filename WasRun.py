3a5b5421c915c820688644d2cf0412eaf5989c92
Ratik Sharma <Ratik96@gmail.com>
2019-03-05 21:04:50 +0530

    Update hard-coded function call to a dynamic testMetho call

--- WasRun.py
+++ WasRun.py
@@ -1,9 +1,11 @@
 class WasRun:
     def __init__(self, name):
         self.was_run = None
+        self.name = name
 
     def testMethod(self):
         self.was_run = 1
 
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
