dfb58f5ed1ed94498efbb758bf3b25b6c1fe883f
Ratik Sharma <Ratik96@gmail.com>
2019-03-05 23:21:04 +0530

    Cleanup - get rid of redundant flags

--- WasRun.py
+++ WasRun.py
@@ -3,11 +3,9 @@ from TestCase import TestCase
 
 class WasRun(TestCase):
     def setup(self):
-        self.was_run = None
         self.log = "setup "
 
     def test_method(self):
-        self.was_run = 1
         self.log += "test_method "
 
     def tear_down(self):
