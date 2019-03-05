b430f29161548e36235165404c4d0b45adf9e0d1
Ratik Sharma <Ratik96@gmail.com>
2019-03-05 23:29:10 +0530

    Add failing test for the interface we wish to build for reporting

--- script.py
+++ script.py
@@ -8,6 +8,12 @@ class TestCaseTest(TestCase):
         self.test.run()
         assert("setup test_method tear_down " == self.test.log)
 
+    def test_result(self):
+        self.test = WasRun("test_method")
+        result = self.test.run()
+        assert("1 run, 0 failed" == result.summary())
+
 
 if __name__ == "__main__":
     TestCaseTest("test_template_method").run()
+    TestCaseTest("test_result").run()
